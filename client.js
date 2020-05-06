fb08ee6e68487cd0f7aa447bc3b2d28c6386cd76
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-06 19:59:44 +0000

    Moved connection function to seperate file

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '50.64.116.162',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (message) => {
+    console.log('Message from server', message);
+  });
+
+  return conn;
+}
+
+module.exports = {
+  connect
+};
